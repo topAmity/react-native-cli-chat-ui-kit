48a6ee9cbee870d9e1325fbc14cebcf04f4ae270
topAmity <112688936+topAmity@users.noreply.github.com>
2024-03-19 19:08:59 +0700

    update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { AmityUiKitProvider, AmityUiKitChat } from '@amityco/react-native-cli-chat-ui-kit';
+import { AmityUiKitProvider, AmityUiKitChat } from '@amityco/react-native-chat-ui-kit';
 
 export default function App() {
 
   return (
     <AmityUiKitProvider
-      apiKey="b3babb0b3a89f4341d31dc1a01091edcd70f8de7b23d697f"
-      apiRegion="sg"
-      userId="topAmity"
-      displayName="topAmity"
-      apiEndpoint="https://api.sg.amity.co"
+      apiKey="YOUR_API_KEY" // Put your apiKey
+      apiRegion="API_REGION" // Put your apiRegion
+      userId="USER_ID" // Put your UserId
+      displayName="DISPLAYNAME" // Put your displayName
+      apiEndpoint="API_ENDPOINT" //"https://api.{apiRegion}.amity.co" e.g "https://api.eu.amity.co"
     >
       <AmityUiKitChat />
     </AmityUiKitProvider>
